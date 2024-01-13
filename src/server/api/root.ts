bdcfd30d3b9e3bc911543ac57df5751b36eea161
Aldiery <aldieryrgon@gmail.com>
2024-01-13 17:06:59 -0600

    Chore: Removed Post Router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { eventRouter } from "~/server/api/routers/events";
 import { createTRPCRouter } from "~/server/api/trpc";
 
@@ -8,7 +7,6 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   events: eventRouter,
 });
 
