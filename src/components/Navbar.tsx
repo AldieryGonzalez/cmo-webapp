ab77a784a2acdda0cab4b022d8e4598c75cfcb58
Aldiery <aldieryrgon@gmail.com>
2024-01-13 17:36:42 -0600

    Fix: Dashboard link now goes to '/'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,17 +13,6 @@ export default async function Navbar() {
         </Link>
         <UserContents user={user} />
       </nav>
-      // <div className="bg-popover border-b mb-2 md:p-0 px-4">
-      // <nav className="py-2 flex items-center justify-between transition-all duration-300 max-w-3xl mx-auto">
-      //   <h1 className="font-semibold hover:opacity-75 transition-hover cursor-pointer">
-      //     <Link href="/">Logo</Link>
-      //   </h1>
-      //   <div className="space-x-2 flex items-center">
-      //     <ModeToggle />
-      //     <UserButton afterSignOutUrl="/" />
-      //   </div>
-      // </nav>
-      // </div>
     );
-  } else return "No user?";
+  }
 }
