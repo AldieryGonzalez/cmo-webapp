763a7860348bcacf7c9f7c6f04335fa9ca3bd9ed
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-09 16:55:49 -0600

    Fix: MyShifts not being able to use getUser Issue was that MyShifts is behind Client Boundary I was avoiding using the string array because I might want the entire user, but it cant be passed from Server to Client component. This is because it was too complex The solution that was causing problems was trying to use getUser in myShifts, which while not specifically a client component, is rendered by one. This means it does not get access to the server component clerk functions. Solution: prop drill the alternative Names/ searchNames array, which can be passed server to client

--- src/app/shifts/_components/MyShifts.tsx
+++ src/app/shifts/_components/MyShifts.tsx
@@ -1,95 +1,100 @@
 import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
 } from "~/components/ui/card";
 import { TabsContent } from "~/components/ui/tabs";
 import { groupEventsByDay } from "~/lib/gcal/utils";
 
-import { currentUser } from "@clerk/nextjs";
-import { type User } from "@clerk/nextjs/server";
 import Link from "next/link";
-import { contactToSearchNames, getUser, nameToContact } from "~/lib/auth/utils";
 import { inEvent, roleInEvent, timeRangeString } from "~/lib/events/utils";
 import type { EventsOutput } from "~/server/api/routers/events";
 
 type CmoEvent = EventsOutput["getEvents"][0];
 type OverviewProps = {
-  events: CmoEvent[];
+    events: CmoEvent[];
+    searchNames: string[];
 };
 
 type DaySectionProps = {
-  day: string;
-  events: CmoEvent[];
-  names: string[];
+    day: string;
+    events: CmoEvent[];
+    searchNames: string[];
 };
 
 type ShiftCardProps = {
-  event: CmoEvent;
-  names: string[];
+    event: CmoEvent;
+    searchNames: string[];
 };
 
-const DaySection: React.FC<DaySectionProps> = ({ day, events, names }) => {
-  return (
-    <div className="space-y-1">
-      <h3 className="text-lg font-semibold">{day}</h3>
-      {events.map((event) => (
-        <ShiftCard key={event.id} event={event} names={names} />
-      ))}
-    </div>
-  );
+const DaySection: React.FC<DaySectionProps> = ({
+    day,
+    events,
+    searchNames,
+}) => {
+    return (
+        <div className="space-y-1">
+            <h3 className="text-lg font-semibold">{day}</h3>
+            {events.map((event) => (
+                <ShiftCard
+                    key={event.id}
+                    event={event}
+                    searchNames={searchNames}
+                />
+            ))}
+        </div>
+    );
 };
 
-const ShiftCard: React.FC<ShiftCardProps> = ({ event, names }) => {
-  return (
-    <Card className="transition-all hover:scale-x-[1.01] hover:scale-y-105 hover:shadow-lg">
-      <Link href={`/shifts/${event.id}`} className="block h-full w-full">
-        <CardHeader className="space-y-0 px-4 py-2.5">
-          {!event.cancelled ? (
-            <CardTitle className="text-lg">{`${event.title}`}</CardTitle>
-          ) : (
-            <CardTitle className="text-lg text-red-900 line-through">{`${event.title}`}</CardTitle>
-          )}
-          <CardDescription>
-            <b className="font-semibold">{`${roleInEvent(event, names)}`}</b>
-            {`${
-              event.location !== undefined ? ` - ${event.location} - ` : ""
-            }${timeRangeString(event)}`}
-          </CardDescription>
-        </CardHeader>
-      </Link>
-    </Card>
-  );
+const ShiftCard: React.FC<ShiftCardProps> = ({ event, searchNames }) => {
+    return (
+        <Card className="transition-all hover:scale-x-[1.01] hover:scale-y-105 hover:shadow-lg">
+            <Link href={`/shifts/${event.id}`} className="block h-full w-full">
+                <CardHeader className="space-y-0 px-4 py-2.5">
+                    {!event.cancelled ? (
+                        <CardTitle className="text-lg">{`${event.title}`}</CardTitle>
+                    ) : (
+                        <CardTitle className="text-lg text-red-900 line-through">{`${event.title}`}</CardTitle>
+                    )}
+                    <CardDescription>
+                        <b className="font-semibold">{`${roleInEvent(
+                            event,
+                            searchNames,
+                        )}`}</b>
+                        {`${
+                            event.location !== undefined
+                                ? ` - ${event.location} - `
+                                : ""
+                        }${timeRangeString(event)}`}
+                    </CardDescription>
+                </CardHeader>
+            </Link>
+        </Card>
+    );
 };
 
-const MyShifts: React.FC<OverviewProps> = async ({ events }) => {
-  // const user = await getUser();
-  const user = await currentUser();
-  if (!user) return null;
-  const contact = nameToContact(user.firstName + " " + user.lastName);
-  if (!contact) return null;
-  const names = contactToSearchNames(contact);
-  const myEvents = groupEventsByDay(
-    events.filter((event) => inEvent(event, names)),
-  );
-  return (
-    <TabsContent value="myShifts" className="space-y-4">
-      <div className="mx-2 mb-5 flex flex-col gap-2 ">
-        <h3 className="text-3xl font-bold">Your Shifts</h3>
-        {myEvents.map((day) => {
-          return (
-            <DaySection
-              key={day.day}
-              day={day.day}
-              events={day.events}
-              names={names}
-            />
-          );
-        })}
-      </div>
-    </TabsContent>
-  );
+const MyShifts: React.FC<OverviewProps> = async ({ events, searchNames }) => {
+    const myEvents = groupEventsByDay(
+        events.filter((event) => inEvent(event, searchNames)),
+    );
+    return (
+        <TabsContent value="myShifts" className="space-y-4">
+            <div className="mx-2 mb-5 flex flex-col gap-2 ">
+                <h3 className="text-3xl font-bold">Your Shifts</h3>
+                {myEvents.map((day) => {
+                    return (
+                        <DaySection
+                            key={day.day}
+                            day={day.day}
+                            events={day.events}
+                            searchNames={searchNames}
+                        />
+                    );
+                })}
+            </div>
+        </TabsContent>
+    );
 };
 
 export default MyShifts;
--- src/app/shifts/page.tsx
+++ src/app/shifts/page.tsx
@@ -8,31 +8,35 @@ import ShiftDatePicker from "./_components/ShiftDatePicker";
 import TabContainer from "./_components/TabContainer";
 
 const Shifts = async ({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: Record<string, string | undefined>;
+    searchParams: Record<string, string | undefined>;
 }) => {
-  searchParams.start = searchParams.start ?? new Date().toISOString();
-  searchParams.end = searchParams.end ?? addMonths(new Date(), 3).toISOString();
-  const start = new Date(searchParams.start);
-  const end = new Date(searchParams.end);
-  const data = await api.events.getEvents.query({ start, end });
-  const user = await getUser();
-  if (!user) return null;
-  const events = data.filter((event) => {
-    return isSearched(event, searchParams, user.searchNames);
-  });
-  console.log(events);
+    searchParams.start = searchParams.start ?? new Date().toISOString();
+    searchParams.end =
+        searchParams.end ?? addMonths(new Date(), 3).toISOString();
+    const start = new Date(searchParams.start);
+    const end = new Date(searchParams.end);
+    const data = await api.events.getEvents.query({ start, end });
+    const user = await getUser();
+    if (!user) return null;
+    const events = data.filter((event) => {
+        return isSearched(event, searchParams, user.searchNames);
+    });
 
-  return (
-    <div className="flex-1 space-y-4 p-5 pt-6">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-semibold">Shifts</h1>
-        <ShiftDatePicker searchParams={searchParams} />
-      </div>
-      <TabContainer events={events} searchParams={searchParams} />
-    </div>
-  );
+    return (
+        <div className="flex-1 space-y-4 p-5 pt-6">
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-semibold">Shifts</h1>
+                <ShiftDatePicker searchParams={searchParams} />
+            </div>
+            <TabContainer
+                events={events}
+                searchParams={searchParams}
+                searchNames={user.searchNames}
+            />
+        </div>
+    );
 };
 
 export default Shifts;
