8be83ee765e363870e58659c62ae8d9676695c7f
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-09 18:38:45 -0600

    Feat: Using Mailparser to make returns simpler/more stable

--- src/components/dashboard/DashboardMessages.tsx
+++ src/components/dashboard/DashboardMessages.tsx
@@ -2,63 +2,66 @@
 import { format, formatDistanceToNow } from "date-fns";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useState } from "react";
+import { type MessagesOutput } from "~/server/api/routers/messages";
 
 type Props = {
-  messages: ({
-    subject: string;
-    from: string;
-    date: string;
-    to: string;
-    payload: string | null;
-  } | null)[];
+    messages: MessagesOutput["getEvents"];
 };
 
-const formatDate = (date: string) => {
-  const formattedDate = format(date, "MMM d, yyyy, h:mm a");
-  const hoursAgo = formatDistanceToNow(date, { addSuffix: true });
-  return `${formattedDate} (${hoursAgo})`;
+const formatDate = (date: Date) => {
+    const formattedDate = format(date, "MMM d, yyyy, h:mm a");
+    const hoursAgo = formatDistanceToNow(date, { addSuffix: true });
+    return `${formattedDate} (${hoursAgo})`;
 };
 
 const DashboardMessages = ({ messages }: Props) => {
-  const [index, setIndex] = useState(0);
-  return (
-    <section className="flex max-h-full grow flex-col">
-      <h3 className="grow-[2] text-xl font-normal">Announcements</h3>
-      <div className="flex grow flex-col overflow-y-auto rounded-md border border-card-foreground/35 bg-card shadow-sm shadow-primary/10">
-        <nav className="flex h-7 grow-[2] justify-end gap-2 bg-zinc-400 px-6 py-0.5">
-          <p>{`${index + 1} of ${messages.length}`}</p>
-          <button
-            onClick={() =>
-              setIndex((prev) =>
-                prev - 1 < 0 ? messages.length - 1 : prev - 1,
-              )
-            }
-          >
-            <ChevronLeft />
-          </button>
-          <button
-            onClick={() => setIndex((prev) => (prev + 1) % messages.length)}
-          >
-            <ChevronRight />
-          </button>
-        </nav>
-        <div className="grow overflow-y-auto">
-          <div className="flex max-h-full flex-col overflow-auto p-3">
-            <p className="text-lg font-semibold">{messages[index]?.subject}</p>
-            <p className="text-sm font-normal">{messages[index]?.from}</p>
-            <p className="text-sm font-normal">To: {messages[index]?.to}</p>
-            <p className="text-sm font-normal">{`Sent: ${formatDate(
-              messages[index]?.date ?? "0",
-            )}`}</p>
-            {/* <p className="text-sm font-normal">{`Sent: ${formatDate(date)}`}</p> */}
-            <pre className="whitespace-pre-wrap text-sm font-normal">
-              {messages[index]?.payload ?? "No message"}
-            </pre>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+    const [index, setIndex] = useState(0);
+    return (
+        <section className="flex max-h-full grow flex-col">
+            <h3 className="grow-[2] text-xl font-normal">Announcements</h3>
+            <div className="flex grow flex-col overflow-y-auto rounded-md border border-card-foreground/35 bg-card shadow-sm shadow-primary/10">
+                <nav className="flex h-7 grow-[2] justify-end gap-2 bg-zinc-400 px-6 py-0.5">
+                    <p>{`${index + 1} of ${messages.length}`}</p>
+                    <button
+                        onClick={() =>
+                            setIndex((prev) =>
+                                prev - 1 < 0 ? messages.length - 1 : prev - 1,
+                            )
+                        }
+                    >
+                        <ChevronLeft />
+                    </button>
+                    <button
+                        onClick={() =>
+                            setIndex((prev) => (prev + 1) % messages.length)
+                        }
+                    >
+                        <ChevronRight />
+                    </button>
+                </nav>
+                <div className="grow overflow-y-auto">
+                    <div className="flex max-h-full flex-col overflow-auto p-3">
+                        <p className="text-lg font-semibold">
+                            {messages[index]?.subject}
+                        </p>
+                        <p className="text-sm font-normal">
+                            {messages[index]?.from}
+                        </p>
+                        <p className="text-sm font-normal">
+                            To: {messages[index]?.to}
+                        </p>
+                        <p className="text-sm font-normal">{`Sent: ${formatDate(
+                            messages[index]?.date ?? new Date(0),
+                        )}`}</p>
+                        {/* <p className="text-sm font-normal">{`Sent: ${formatDate(date)}`}</p> */}
+                        <pre className="whitespace-pre-wrap text-sm font-normal">
+                            {messages[index]?.text ?? "No message"}
+                        </pre>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default DashboardMessages;
--- src/server/api/routers/messages.ts
+++ src/server/api/routers/messages.ts
@@ -1,63 +1,96 @@
 import { TRPCError, type inferRouterOutputs } from "@trpc/server";
+import { simpleParser } from "mailparser";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedGapiProcedure } from "~/server/api/trpc";
 
 export const messageRouter = createTRPCRouter({
-  getEvents: protectedGapiProcedure
-    .input(
-      z
-        .object({
-          start: z.date(),
-          end: z.date(),
-        })
-        .optional(),
-    )
-    .query(async ({ ctx }) => {
-      const listResponse = await ctx.gmail.users.messages.list({
-        userId: "me",
-        maxResults: 10,
-        q: "from:(w-milgram@northwestern.edu) to:(tech-staff@listserv.it.northwestern.edu)",
-      });
-      if (listResponse.status !== 200 || !listResponse.data.messages) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Failed to fetch messages",
-        });
-      }
-      //   return listResponse.data.messages;
-      //   const id = messages.data.messages[0]?.id;
-      const messagePromises = listResponse.data.messages.map(
-        async (message) => {
-          const id = message.id ?? undefined;
-          const messageResponse = await ctx.gmail.users.messages.get({
-            userId: "me",
-            id,
-            format: "full",
-          });
-          if (messageResponse.status !== 200 || !messageResponse.data) {
-            throw new TRPCError({
-              code: "INTERNAL_SERVER_ERROR",
-              message: `Failed to fetch individual message ${message.id}`,
-              cause: messageResponse,
+    getEvents: protectedGapiProcedure
+        .input(
+            z
+                .object({
+                    start: z.date(),
+                    end: z.date(),
+                })
+                .optional(),
+        )
+        .query(async ({ ctx }) => {
+            const listResponse = await ctx.gmail.users.messages.list({
+                userId: "me",
+                maxResults: 10,
+                q: "from:(w-milgram@northwestern.edu) to:(tech-staff@listserv.it.northwestern.edu)",
             });
-          }
-          return messageResponse.data;
-        },
-      );
-      const messages = await Promise.all(messagePromises)
-        .then((message) => {
-          return message;
-        })
-        .catch((error) => {
-          throw new TRPCError({
-            code: "INTERNAL_SERVER_ERROR",
-            message: "Failed to fetch individual messages",
-            cause: error,
-          });
-        });
-      return messages;
-    }),
+            if (listResponse.status !== 200 || !listResponse.data.messages) {
+                throw new TRPCError({
+                    code: "INTERNAL_SERVER_ERROR",
+                    message: "Failed to fetch messages",
+                });
+            }
+            //   return listResponse.data.messages;
+            //   const id = messages.data.messages[0]?.id;
+            const messagePromises = listResponse.data.messages.map(
+                async (message) => {
+                    const id = message.id ?? undefined;
+                    const messageResponse = await ctx.gmail.users.messages.get({
+                        userId: "me",
+                        id,
+                        format: "raw",
+                    });
+                    if (
+                        messageResponse.status !== 200 ||
+                        !messageResponse.data
+                    ) {
+                        throw new TRPCError({
+                            code: "INTERNAL_SERVER_ERROR",
+                            message: `Failed to fetch individual message ${message.id}`,
+                            cause: messageResponse,
+                        });
+                    }
+                    if (!messageResponse.data.raw) {
+                        throw new TRPCError({
+                            code: "INTERNAL_SERVER_ERROR",
+                            message: `No raw data for message ${message.id}`,
+                            cause: messageResponse,
+                        });
+                    }
+                    const decodedMessage = Buffer.from(
+                        messageResponse.data.raw,
+                        "base64",
+                    ).toString("utf-8");
+                    const parsed = await simpleParser(decodedMessage);
+                    return parsed;
+                },
+            );
+            const messages = await Promise.all(messagePromises)
+                .then((messages) => {
+                    return messages.map((message) => {
+                        const attatchments = message.attachments || [];
+                        const to = Array.isArray(message.to)
+                            ? message.to.map(
+                                  (to) => to.value[0]?.address ?? "Unknown",
+                              )
+                            : [message.to?.value[0]?.address ?? "Unknown"];
+                        return {
+                            id: message.messageId,
+                            from: message.from?.value[0]?.address ?? "Unknown",
+                            to: to,
+                            subject: message.subject,
+                            date: message.date,
+                            text: message.text,
+                            html: message.html,
+                            attatchments: attatchments,
+                        };
+                    });
+                })
+                .catch((error) => {
+                    throw new TRPCError({
+                        code: "INTERNAL_SERVER_ERROR",
+                        message: "Failed to fetch individual messages",
+                        cause: error,
+                    });
+                });
+            return messages;
+        }),
 });
 export type MessagesRouter = typeof messageRouter;
 export type MessagesOutput = inferRouterOutputs<MessagesRouter>;
