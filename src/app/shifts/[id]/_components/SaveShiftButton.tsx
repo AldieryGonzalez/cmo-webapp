e325aa4a03c166b642bbc7fe7531fd433319e9ad
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-10 04:00:37 -0600

    Feat: QOL on Alerts for adding to Cart Closes modal on promise complete and gives link to cart

--- src/app/shifts/[id]/_components/SaveShiftButton.tsx
+++ src/app/shifts/[id]/_components/SaveShiftButton.tsx
@@ -1,25 +1,26 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "~/components/ui/dialog";
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
 } from "~/components/ui/form";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 import { hasPast, stringify } from "~/lib/events/utils";
@@ -28,116 +29,133 @@ import type { Event } from "~/server/api/routers/events";
 import { api } from "~/trpc/react";
 
 type SaveShiftButtonProps = {
-  event: Event;
-  mobile?: boolean;
+    event: Event;
+    mobile?: boolean;
 };
 
 const SaveShiftButton: React.FC<SaveShiftButtonProps> = ({
-  event,
-  mobile = false,
+    event,
+    mobile = false,
 }) => {
-  const canSaveShift = !hasPast(event);
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button
-          disabled={!canSaveShift}
-          variant={"nu"}
-          className={cn(
-            { "hidden sm:inline-flex": !mobile },
-            { "sm:hidden": mobile },
-            "disabled:cursor-not-allowed",
-          )}
-        >
-          <span>Save Shift</span>
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>What shift?</DialogTitle>
-          <DialogDescription>
-            <SaveForm event={event} />
-          </DialogDescription>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
-  );
+    const [open, setOpen] = useState(false);
+    const canSaveShift = !hasPast(event);
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                <Button
+                    disabled={!canSaveShift}
+                    variant={"nu"}
+                    className={cn(
+                        { "hidden sm:inline-flex": !mobile },
+                        { "sm:hidden": mobile },
+                        "disabled:cursor-not-allowed",
+                    )}
+                >
+                    <span>Save Shift</span>
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>What shift?</DialogTitle>
+                    <DialogDescription>
+                        <SaveForm event={event} setOpen={setOpen} />
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
 };
 
-const SaveForm = ({ event }: { event: Event }) => {
-  const FormSchema = z.object({
-    shift: z.string({
-      required_error: "You need to select a notification type.",
-    }),
-  });
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-  });
-  const saveShift = api.events.saveShift.useMutation();
-  async function onSubmit(data: z.infer<typeof FormSchema>) {
-    const findshift = event.shifts.find((shift) => shift.id === data.shift);
-    if (!findshift) {
-      toast.error("Internal error. Please try again.");
-      return;
-    }
-    //removing id so cart event generates new id
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { id, ...shift } = findshift;
-    const res = saveShift.mutateAsync(shift);
-    toast.promise(res, {
-      loading: "Loading...",
-      success: () => {
-        return `${shift.role} shift has been saved`;
-      },
-      error: "Error",
+const SaveForm = ({
+    event,
+    setOpen,
+}: {
+    event: Event;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+    const FormSchema = z.object({
+        shift: z.string({
+            required_error: "You need to select a notification type.",
+        }),
     });
-  }
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-2/3 space-y-6 p-4"
-      >
-        <FormField
-          control={form.control}
-          name="shift"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="flex flex-col space-y-1"
-                >
-                  {event.shifts.map((shift) => {
-                    return (
-                      <FormItem
-                        key={shift.id}
-                        className="flex items-center space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <RadioGroupItem
-                            disabled={shift.isFilled}
-                            value={shift.id}
-                          />
-                        </FormControl>
-                        <FormLabel className="font-normal">
-                          {stringify(shift)}
-                        </FormLabel>
-                      </FormItem>
-                    );
-                  })}
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+    });
+    const saveShift = api.events.saveShift.useMutation();
+    async function onSubmit(data: z.infer<typeof FormSchema>) {
+        const findshift = event.shifts.find((shift) => shift.id === data.shift);
+        if (!findshift) {
+            toast.error("Internal error. Please try again.");
+            return;
+        }
+        //removing id so cart event generates new id
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { id, ...shift } = findshift;
+        const res = saveShift.mutateAsync(shift);
+        toast.promise(res, {
+            loading: "Loading...",
+            success: () => {
+                setOpen(false);
+                return (
+                    <div className="flex w-full justify-between">
+                        {`${shift.role} shift has been saved.  `}
+                        <Link href="/cart" className="font-bold underline">
+                            Go to cart
+                        </Link>
+                    </div>
+                );
+            },
+            error: "Error",
+        });
+    }
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="w-2/3 space-y-6 p-4"
+            >
+                <FormField
+                    control={form.control}
+                    name="shift"
+                    render={({ field }) => (
+                        <FormItem className="space-y-3">
+                            <FormControl>
+                                <RadioGroup
+                                    onValueChange={field.onChange}
+                                    defaultValue={field.value}
+                                    className="flex flex-col space-y-1"
+                                >
+                                    {event.shifts.map((shift) => {
+                                        return (
+                                            <FormItem
+                                                key={shift.id}
+                                                className="flex items-center space-x-3 space-y-0"
+                                            >
+                                                <FormControl>
+                                                    <RadioGroupItem
+                                                        disabled={
+                                                            shift.isFilled
+                                                        }
+                                                        value={shift.id}
+                                                    />
+                                                </FormControl>
+                                                <FormLabel className="font-normal">
+                                                    {stringify(shift)}
+                                                </FormLabel>
+                                            </FormItem>
+                                        );
+                                    })}
+                                </RadioGroup>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
 
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  );
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
 };
 
 export default SaveShiftButton;
--- src/app/shifts/[id]/_components/SaveSpecificShiftButton.tsx
+++ src/app/shifts/[id]/_components/SaveSpecificShiftButton.tsx
@@ -2,40 +2,48 @@
 
 import { isPast } from "date-fns";
 import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
 import { toast } from "sonner";
 import { type Shift } from "~/lib/events/utils";
 import { api } from "~/trpc/react";
 
 type SaveSpecificShiftButtonProps = {
-  shift: Shift;
+    shift: Shift;
 };
 
 const SaveSpecificShiftButton: React.FC<SaveSpecificShiftButtonProps> = ({
-  shift,
+    shift,
 }) => {
-  const saveShift = api.events.saveShift.useMutation();
-  const canAddToCart = !isPast(shift.end) && !shift.isFilled;
-  const addToCart = async () => {
-    const res = saveShift.mutateAsync(shift);
-    toast.promise(res, {
-      loading: "Loading...",
-      success: () => {
-        return `${shift.role} shift has been saved`;
-      },
-      error: "Error",
-    });
-  };
+    const saveShift = api.events.saveShift.useMutation();
+    const canAddToCart = !isPast(shift.end) && !shift.isFilled;
+    const addToCart = async () => {
+        const res = saveShift.mutateAsync(shift);
+        toast.promise(res, {
+            loading: "Loading...",
+            success: () => {
+                return (
+                    <div className="flex w-full justify-between">
+                        {`${shift.role} shift has been saved.  `}
+                        <Link href="/cart" className="font-bold underline">
+                            Go to cart
+                        </Link>
+                    </div>
+                );
+            },
+            error: "Error",
+        });
+    };
 
-  return (
-    <button
-      disabled={!canAddToCart}
-      onClick={addToCart}
-      className="flex w-52 items-center gap-3 rounded text-left text-purple-800 hover:bg-black/20 disabled:cursor-not-allowed disabled:opacity-50"
-    >
-      <ShoppingCart size={20} />
-      Add to Cart
-    </button>
-  );
+    return (
+        <button
+            disabled={!canAddToCart}
+            onClick={addToCart}
+            className="flex w-52 items-center gap-3 rounded text-left text-purple-800 hover:bg-black/20 disabled:cursor-not-allowed disabled:opacity-50"
+        >
+            <ShoppingCart size={20} />
+            Add to Cart
+        </button>
+    );
 };
 
 export default SaveSpecificShiftButton;
