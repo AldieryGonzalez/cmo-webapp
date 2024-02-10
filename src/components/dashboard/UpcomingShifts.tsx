956f20f2294692d46b2f3d036794bdfdb3dacc2b
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-10 03:35:37 -0600

    Feat: Changed to OpenShifts when recommending

--- src/components/dashboard/UpcomingShifts.tsx
+++ src/components/dashboard/UpcomingShifts.tsx
@@ -6,45 +6,51 @@ import { type EventsOutput } from "~/server/api/routers/events";
 import DashboardShiftCard from "./DashboardShiftCard";
 
 type Props = {
-  events: EventsOutput["getEvents"];
-  user: namedUser;
+    events: EventsOutput["getEvents"];
+    user: namedUser;
 };
 interface namedUser extends User {
-  searchNames: string[];
+    searchNames: string[];
 }
 
 const UpcomingShifts = ({ events, user }: Props) => {
-  const upcomingShifts = events.filter((event) => {
-    return inEvent(event, user.searchNames) && isAfter(event.start, new Date());
-  });
-  if (upcomingShifts.length === 0) {
+    const upcomingShifts = events.filter((event) => {
+        return (
+            inEvent(event, user.searchNames) && isAfter(event.start, new Date())
+        );
+    });
+    if (upcomingShifts.length === 0) {
+        return (
+            <div className="mt-4 w-full rounded-lg border border-card-foreground/35 bg-card p-4 shadow">
+                <h3 className="text-base">
+                    No upcoming shifts, go to{" "}
+                    <Link
+                        href={"/shifts?shifts=openShifts"}
+                        className="font-semibold text-purple-900 underline dark:text-purple-600"
+                    >
+                        shifts
+                    </Link>{" "}
+                    to search for and sign up for shifts.
+                </h3>
+            </div>
+        );
+    }
     return (
-      <div className="mt-4 w-full rounded-lg border border-card-foreground/35 bg-card p-4 shadow">
-        <h3 className="text-base">
-          No upcoming shifts, go to{" "}
-          <Link
-            href={"/shifts?shifts=allShifts"}
-            className="font-semibold text-purple-900 underline dark:text-purple-600"
-          >
-            shifts
-          </Link>{" "}
-          to search for and sign up for shifts.
-        </h3>
-      </div>
+        <div className="w-full">
+            <h3 className="text-xl font-normal">Upcoming Shifts</h3>
+            <div className="flex snap-x snap-mandatory scroll-p-4 gap-5 overflow-x-auto pb-2">
+                {upcomingShifts.map((event) => {
+                    return (
+                        <DashboardShiftCard
+                            key={event.id}
+                            event={event}
+                            user={user}
+                        />
+                    );
+                })}
+            </div>
+        </div>
     );
-  }
-  return (
-    <div className="w-full">
-      <h3 className="text-xl font-normal">Upcoming Shifts</h3>
-      <div className="flex snap-x snap-mandatory scroll-p-4 gap-5 overflow-x-auto pb-2">
-        {upcomingShifts.map((event) => {
-          return (
-            <DashboardShiftCard key={event.id} event={event} user={user} />
-          );
-        })}
-      </div>
-    </div>
-  );
 };
 
 export default UpcomingShifts;
