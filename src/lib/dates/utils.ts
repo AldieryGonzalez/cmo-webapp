3e572f085f3fd37232833f3c608fc538bfd51353
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-12 07:12:30 -0600

    Fix: TimeZone for shift times?

--- src/lib/dates/utils.ts
+++ src/lib/dates/utils.ts
@@ -1,22 +1,25 @@
 export function timeStringToDate(date: Date, timeString: string): Date {
-  const defaultHour = date.getHours();
-  const defaultMinute = date.getMinutes();
-  const timeMatch = timeString?.match(/(\d{1,2}):(\d{2})([ap]m)/);
-  if (timeMatch) {
-    let hours = timeMatch[1];
-    const minutes = timeMatch[2];
-    const locale = timeMatch[3];
-    if (locale === "pm") {
-      hours = (parseInt(hours ?? defaultHour.toString()) + 12).toString();
-    }
-    return new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
-      parseInt(hours ?? defaultHour.toString()),
-      parseInt(minutes ?? defaultMinute.toString()),
+    const chicagoDate = new Date(
+        date.toLocaleString("en-US", { timeZone: "America/Chicago" }),
     );
-  } else {
-    return date;
-  }
+    const defaultHour = chicagoDate.getHours();
+    const defaultMinute = chicagoDate.getMinutes();
+    const timeMatch = timeString?.match(/(\d{1,2}):(\d{2})([ap]m)/);
+    if (timeMatch) {
+        let hours = timeMatch[1];
+        const minutes = timeMatch[2];
+        const locale = timeMatch[3];
+        if (locale === "pm") {
+            hours = (parseInt(hours ?? defaultHour.toString()) + 12).toString();
+        }
+        return new Date(
+            chicagoDate.getFullYear(),
+            chicagoDate.getMonth(),
+            chicagoDate.getDate(),
+            parseInt(hours ?? defaultHour.toString()),
+            parseInt(minutes ?? defaultMinute.toString()),
+        );
+    } else {
+        return chicagoDate;
+    }
 }
