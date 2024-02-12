be970755058915b7bda0d430dec01d8b4e13b3cd
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-12 05:44:01 -0600

    apiTests change

--- src/app/apiTests/page.tsx
+++ src/app/apiTests/page.tsx
@@ -2,14 +2,21 @@ import { addMonths } from "date-fns";
 import { api } from "~/trpc/server";
 
 const Tester = async () => {
-    const freeBusy = await api.events.freeBusy.query({
-        start: new Date(),
-        end: addMonths(new Date(), 6),
+    // Date of feb 23, 2024
+    const event = await api.events.getEvent.query("1p93ffa7is47g3c4ajhmp7ap04");
+    const events = await api.events.getEvents.query({
+        start: new Date("2024-02-23"),
+        end: new Date("2024-02-25"),
     });
 
     return (
-        <div className="whitespace-pre">
-            {JSON.stringify(freeBusy, undefined, 4)}
+        <div>
+            <div className="whitespace-pre bg-white">
+                {JSON.stringify(event, undefined, 4)}
+            </div>
+            <div className="whitespace-pre">
+                {JSON.stringify(events, undefined, 4)}
+            </div>
         </div>
     );
 };
