349f4f178527f60c0a53c00d16370b4d42daa7c0
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-12 07:47:52 -0600

    Test Commit

--- src/lib/dates/utils.ts
+++ src/lib/dates/utils.ts
@@ -2,18 +2,28 @@ export function timeStringToDate(date: Date, timeString: string): Date {
     const chicagoDate = new Date(
         date.toLocaleString("en-US", { timeZone: "America/Chicago" }),
     );
+    const timeMatch = timeString.match(/(\d+)(?::(\d+))?(am|pm)?/i);
     const defaultHour = chicagoDate.getHours();
     const defaultMinute = chicagoDate.getMinutes();
-    const timeMatch = timeString?.match(/(\d{1,2}):(\d{2})([ap]m)/);
+    // const timeMatch = timeString?.match(/(\d{1,2}):(\d{2})([ap]m)/);
     if (timeMatch) {
-        let hours = timeMatch[1];
-        const minutes = timeMatch[2];
-        const locale = timeMatch[3];
-        if (locale === "pm") {
-            hours = (parseInt(hours ?? defaultHour.toString()) + 12).toString();
+        let hours = timeMatch[1] ? parseInt(timeMatch[1], 10) : defaultHour;
+        const minutes = timeMatch[2]
+            ? parseInt(timeMatch[2], 10)
+            : defaultMinute;
+        const period = timeMatch[3] ? timeMatch[3].toLowerCase() : undefined;
+        if (period === "pm" && hours !== 12) {
+            hours += 12;
+        } else if (period === "am" && hours === 12) {
+            hours = 0;
         }
-        chicagoDate.setHours(parseInt(hours ?? defaultHour.toString()));
-        chicagoDate.setMinutes(parseInt(minutes ?? defaultMinute.toString()));
+        // let hours = timeMatch[1];
+        // const minutes = timeMatch[2];
+        // const locale = timeMatch[3];
+        // if (locale === "pm") {
+        //     hours = (parseInt(hours ?? defaultHour.toString()) + 12).toString();
+        // }
+        chicagoDate.setHours(hours, minutes);
         const formattedDate = new Intl.DateTimeFormat("en-US", {
             timeZone: "America/Chicago",
             year: "numeric",
@@ -25,6 +35,6 @@ export function timeStringToDate(date: Date, timeString: string): Date {
         }).format(chicagoDate);
         return new Date(formattedDate);
     } else {
-        return chicagoDate;
+        return date;
     }
 }
