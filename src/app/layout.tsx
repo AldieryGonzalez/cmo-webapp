19f8f304bab8c86cb8ad62bf40530fdcabb87892
Aldiery Gonzalez <aldieryrgon@gmail.com>
2024-02-16 04:45:29 -0600

    Feat: Add Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
-import "~/styles/globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
+import "~/styles/globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { neobrutalism } from "@clerk/themes";
@@ -11,45 +11,48 @@ import { Toaster } from "~/components/ui/sonner";
 import { TRPCReactProvider } from "~/trpc/react";
 
 const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    variable: "--font-sans",
 });
 
 export const metadata = {
-  title: "Northwestern CMO",
-  description: "Northwestern's Concert Management Office application",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "Northwestern CMO",
+    description: "Northwestern's Concert Management Office application",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider cookies={cookies().toString()}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <ClerkProvider
-              appearance={{
-                baseTheme: neobrutalism,
-              }}
-            >
-              <div className="flex h-svh flex-col">
-                <Navbar />
-                <main className="grow overflow-y-auto">{children}</main>
-              </div>
-            </ClerkProvider>
-            <Toaster />
-          </ThemeProvider>
-        </TRPCReactProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <SpeedInsights />
+            <body className={`font-sans ${inter.variable}`}>
+                <TRPCReactProvider cookies={cookies().toString()}>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        <ClerkProvider
+                            appearance={{
+                                baseTheme: neobrutalism,
+                            }}
+                        >
+                            <div className="flex h-svh flex-col">
+                                <Navbar />
+                                <main className="grow overflow-y-auto">
+                                    {children}
+                                </main>
+                            </div>
+                        </ClerkProvider>
+                        <Toaster />
+                    </ThemeProvider>
+                </TRPCReactProvider>
+            </body>
+        </html>
+    );
 }
